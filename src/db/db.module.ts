0cdc963a79a17767ce6862ed221bc988c5606248
chanweiyan <chanweiyan007@gmail.com>
2024-10-09 12:07:42 +0800

    feat: implement DbModule

--- src/db/db.module.ts
+++ src/db/db.module.ts
@@ -1,7 +1,24 @@
-import { Module } from '@nestjs/common';
+import { DynamicModule, Module } from '@nestjs/common';
 import { DbService } from './db.service';
 
-@Module({
-  providers: [DbService]
-})
-export class DbModule {}
+export interface DbModuleOptions {
+  /** json文件存储路径 */
+  path: string;
+}
+
+@Module({})
+export class DbModule {
+  static register(options: DbModuleOptions): DynamicModule {
+    return {
+      module: DbModule,
+      providers: [
+        {
+          provide: 'OPTIONS',
+          useValue: options,
+        },
+        DbService,
+      ],
+      exports: [DbService]
+    };
+  }
+}
