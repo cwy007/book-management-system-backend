6184e3843a5cc1b36ee091301ca52d9ad0466cb7
chanweiyan <chanweiyan007@gmail.com>
2024-10-09 15:05:15 +0800

    feat: implement userService.register method

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -8,10 +8,8 @@ export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post('register')
-  create(@Body() registerUserDto: RegisterUserDto) {
-    console.log('registerUserDto->', registerUserDto);
-    // return this.userService.create(registerUserDto);
-    return 'done';
+  register(@Body() registerUserDto: RegisterUserDto) {
+    return this.userService.register(registerUserDto);
   }
 
   @Get()
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,9 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Inject, Injectable } from '@nestjs/common';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { RegisterUserDto } from './dto/register-user.dto';
+import { DbService } from 'src/db/db.service';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UserService {
+
+  @Inject(DbService)
+  dbService: DbService;
+
+  async register(registerUserDto: RegisterUserDto) {
+    const users: User[] = await this.dbService.read();
+    const foundUser = users.find(v => v.username === registerUserDto.username);
+    if (foundUser) {
+      throw new BadRequestException('该用户已经注册');
+    }
+
+    const user = new User();
+    user.username = registerUserDto.username
+    user.password = registerUserDto.password;
+    users.push(user);
+
+    await this.dbService.write(users);
+    return user;
+  }
+
   create(createUserDto: RegisterUserDto) {
     return 'This action adds a new user';
   }
